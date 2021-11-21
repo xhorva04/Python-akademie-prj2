1f0e9bf500df68ef85b081a113cd3813dcd7943e
xhorva04 <Tomas@horvath.site>
2021-11-21 19:01:16 +0100

    Prvni snazeni

--- Projekt2-TH.py
+++ Projekt2-TH.py
@@ -1,2 +1,26 @@
-# Projekt 02 – Tomáš H.
-print("Začni")
+# Projekt 02 (Bulls & Cows)– Tomáš H.
+# Import balíčku pro náhodné volby
+import random
+
+def spustit_hru():
+    pass
+
+
+def pozdravit_uzivatele() -> str:
+    print("Vítejte ve hře Bulls & Cows")
+    print("-" * 32)
+    print("Došlo k vygenerování 4ciferného čísla.")
+    print("-" * 32)
+
+
+def prubeh_hry(hadani : bool):
+    un_seznam = random.sample(range(1,9),4)
+    nov = str()
+    for i in un_seznam:
+        nov += str(i)
+    return int(nov)
+
+#     tajne_cislo = random.randint(1000,9999)
+#     return tajne_cislo
+
+print(prubeh_hry(hadani = True))
