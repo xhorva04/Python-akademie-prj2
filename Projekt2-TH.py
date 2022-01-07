ce3442290637bce4f78ba01bd053ee9d77ff2c77
xhorva04 <Tomas@horvath.site>
2022-01-07 17:01:23 +0100

    Projekt 2 – dokonceni pilotni verze

--- Projekt2-TH.py
+++ Projekt2-TH.py
@@ -2,25 +2,28 @@
 # Import balíčku pro náhodné volby
 import random
 
-
+# Spusteni hry
 def spustit_hru():
+    pokusy = 0
     pozdravit_uzivatele()
     tajne_cislo = generovani_tajneho_cisla()
+    while True:
+        zadane_cislo = zadani_cisla()
+        pokusy += 1
+        if vyhodnot_hru(zadane_cislo, tajne_cislo):
+            break
+    print(f"Vyhral jsi")
+    print(f"Pocet pokusu {pokusy}")
     print(tajne_cislo)
-    vyhodnot_hru(zadani_cisla(), tajne_cislo)
-    # while True:
-    #     zadane_cislo = zadani_cisla()
-    #     if vyhodnot_hru(zadane_cislo, tajne_cislo):
-    #         break
 
-
+# Vygeneruje pzodrav uzivatele
 def pozdravit_uzivatele() -> str:
     print("Vítejte ve hře Bulls & Cows")
     print("-" * 32)
     print("Došlo k vygenerování 4ciferného čísla.")
     print("-" * 32)
 
-
+# Prubeh hry
 def prubeh_hry(hadani: bool):
     un_seznam = random.sample(range(1, 9), 4)
     nov = str()
@@ -29,45 +32,32 @@ def prubeh_hry(hadani: bool):
         nov += str(i)
     return int(nov)
 
-
+# Zadani cisla uzivatelem a jeho vyhodnoceni
 def zadani_cisla() -> int:
     zadane_cislo = input("Zadej svuj tip: ")
-    if zadane_cislo.isdigit() and len(zadane_cislo) == 4 and int(zadane_cislo[0]) > 0:
-        return zadane_cislo
+    if zadane_cislo.isdigit() and len(zadane_cislo) == \
+            4 and int(zadane_cislo[0]) > 0:
+        return int(zadane_cislo)
     else:
         print("Zadane cislo nesplnuje podminky, ukončuji program ...")
         quit()
 
-
-def vyhodnot_hru(zadane: int, tajne: int):
-    # if zadane == tajne:
-    #     print("Vyhrál si")
+# Vyhodnoceni hry zadane vs tajne cislo + formatovany vypis souteze
+def vyhodnot_hru(zadane: int, tajne: int) -> bool:
     bull = 0
     cow = 0
-
-    # Tohle nize funguje na bull
-    # for i, prvek in enumerate(str(zadane)):
-    #     for x, cislo in enumerate(str(tajne)):
-    #         if (i, prvek) == (x, cislo):
-    #             bull += 1
-
     for i in range(4):
         if str(tajne)[i] == str(zadane)[i]:
             cow += 1
         if str(zadane)[i] in str(tajne) and str(tajne)[i] != str(zadane)[i]:
             bull += 1
 
-    print(f"{cow} cow + {bull} bull")
-    # # Výpis uhodlých císel se stejnou pozici
-    # if bull > 1:
-    #     print(bull, "bulls")
-    # elif bull == 0:
-    #     pass
-    # else:
-    #     print(bull, "bull")
+    print(f"{cow} cow{'s' if cow > 1 else ''} + {bull} bull{'s' if bull > 1 else ''}")
+    print("-" * 44)
 
+    return tajne == zadane
 
-
+# Generovani tajneho cisla
 def generovani_tajneho_cisla():
     generated = []
     nov = str()
